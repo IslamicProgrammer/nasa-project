23576e20391aa6d1c706ccc89345db8043672c48
Murodjon Azamov <63091703+IslamicProgrammer@users.noreply.github.com>
2023-02-14 07:59:09 +0500

    project setup

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,38 +1,39 @@
-const launches = new Map();
+const launches = new Map()
 
-let latestFlightNumber = 100;
+let latestFlightNumber = 100
 
 const launch = {
   flightNumber: 100,
-  mission: 'Kepler Exploration X',
-  rocket: 'Explorer IS1',
-  launchDate: new Date('December 27, 2030'),
-  target: 'Kepler-442 b',
-  customer: ['ZTM', 'NASA'],
+  mission: "Kepler Exploration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("December 27, 2030"),
+  target: "Kepler-442 b",
+  customer: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
-};
+}
 
-launches.set(launch.flightNumber, launch);
+launches.set(launch.flightNumber, launch)
 
 function getAllLaunches() {
-  return Array.from(launches.values());
+  return Array.from(launches.values())
 }
 
 function addNewLaunch(launch) {
-  latestFlightNumber++;
+  latestFlightNumber++
+
   launches.set(
     latestFlightNumber,
     Object.assign(launch, {
       success: true,
       upcoming: true,
-      customers: ['Zero to Mastery', 'NASA'],
+      customers: ["Zero to Mastery", "NASA"],
       flightNumber: latestFlightNumber,
     })
-  );
+  )
 }
 
 module.exports = {
   getAllLaunches,
   addNewLaunch,
-};
+}
